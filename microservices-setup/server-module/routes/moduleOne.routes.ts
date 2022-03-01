2856a482c56c2e8aa32151781283c820849845ec
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-01 15:18:54 -0500

    Update moduleOne.routes.ts

--- microservices-setup/server-module/routes/moduleOne.routes.ts
+++ microservices-setup/server-module/routes/moduleOne.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import * as LoginController from "../controllers/moduleOne/productionLogin.controllers";
+import * as LoginController from "../controllers/moduleOne/productionLogin.controller";
 
 const moduleOneRouter = express.Router();
 
