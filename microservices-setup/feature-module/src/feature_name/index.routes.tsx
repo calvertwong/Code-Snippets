42e84b728cf41c2ca937fa17ec4902c32e01ee9b
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-09 16:26:57 -0500

    Added comment to include an important reference

--- microservices-setup/feature-module/src/feature_name/index.routes.tsx
+++ microservices-setup/feature-module/src/feature_name/index.routes.tsx
@@ -6,6 +6,7 @@ import { SamplePage } from "./index";
 * Outcome will be something like this: http://localhost:3000/feature/sample-page
 */
 const FeatureRoutes = (): JSX.Element => (
+  {* Refer to https://reactrouter.com/docs/en/v6/getting-started/overview, Descendant <Routes> *}
   <Routes>
     <Route path="sample-page" element={<SamplePage />} />,
   </Routes>
