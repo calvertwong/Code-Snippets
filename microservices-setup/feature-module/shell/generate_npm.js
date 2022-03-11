78c2061570f395b2029977be8b3a3ddf53c70d8f
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-11 14:03:42 -0500

    Update code to support the major change.

--- microservices-setup/feature-module/shell/generate_npm.js
+++ microservices-setup/feature-module/shell/generate_npm.js
@@ -1,16 +1,30 @@
 /**
  * Remember to copy non-Typescript items manually
  */
-
 var shell = require("shelljs");
+
+var SUCCESS = 0;
 
 shell.echo(`Removing dist contents......(${new Date()})`);
 shell.rm("-r", "dist/*");
 shell.echo(`Generating dist for npm`);
-shell.exec("tsc -p tsconfig.dist.json");
+if (shell.exec("tsc -p tsconfig.dist.json").code == SUCCESS) {
+  shell.echo(`dist is generated`);
+  shell.echo(`Reorganizing...`);
 
-shell.exec("copyfiles -u 1 src/**/*.css dist/src");
+  shell.cp("-r", "dist/src/*", "dist");
+  shell.rm("-r", "dist/src");
 
-shell.echo(`dist is generated, done......(${new Date()})`);
+  shell.exec("copyfiles -u 1 src/**/*.css dist");
+  shell.rm("dist/index.css");
+
+  shell.mv("dist/package.dist.json", "dist/package.json");
+  shell.mv("dist/entryFile.js", "dist/index.js");
+  shell.mv("dist/entryFile.d.ts", "dist/index.d.ts");
+
+  shell.echo(`Done......(${new Date()})`);
+} else {
+  shell.echo("Failed to execute tsc!");
+}
 
 shell.exit(0);
