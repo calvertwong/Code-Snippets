e05ed84ab34974db2a0bee09055f42e7caeb5334
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-11 15:45:14 -0500

    Update App.tsx

--- microservices-setup/container-module/src/App.tsx
+++ microservices-setup/container-module/src/App.tsx
@@ -1,11 +1,13 @@
 import {ModuleRoutes} from "some feature module"
 
+const FeatureApp = lazy(() => import("@orgName/featureName"));
+
 <BrowserRouter>
   <Routes>
     <Route index element={<Navigate replace to="production-login" />} />
     <Route path="production-login" element={<ProductionLogin />} />
     
     {* Refer to https://reactrouter.com/docs/en/v6/getting-started/overview, Descendant <Routes> *}
-    <Route path="someModuleName/*" element={<ModuleRoutes />} />
+    <Route path="someModuleName/*" element={<FeatureApp />} />
   </Routes>
 </BrowserRouter>
