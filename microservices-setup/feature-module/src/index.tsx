660cc1f556159a5c270307983365f7e1990969d8
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-11 15:04:23 -0500

    Update index.tsx to support the major change (react router and react redux impl)

--- microservices-setup/feature-module/src/index.tsx
+++ microservices-setup/feature-module/src/index.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
+
+import "./index.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="feature/*" element={<App />} />
+        <Route path="*" element={<div>Error</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
