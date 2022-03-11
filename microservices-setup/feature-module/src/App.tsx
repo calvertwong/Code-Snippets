b3b5dc02fb78bbf168e67f2b0ce0c7f4638387c0
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2022-03-11 15:02:31 -0500

    Update App to support the major change (react redux, react router impl)

--- microservices-setup/feature-module/src/App.tsx
+++ microservices-setup/feature-module/src/App.tsx
@@ -1,1 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+import { combineReducers, createStore, Store } from "redux";
+import { actionFiveReducer } from "./redux/reducers/ActionFiveReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Persistor, persistReducer, persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { Provider } from "react-redux";
+
+const featurePersistConfig = {
+  key: "featureRedux",
+  storage
+};
+
+const featureReducers = combineReducers({
+  actionFiveReducer
+});
+
+const persistedReducer = persistReducer(featurePersistConfig, featureReducers);
+const featureStore = createStore(persistedReducer, composeWithDevTools());
+const featurePersistor: Persistor = persistStore(featureStore);
+type FeatureReduxState = ReturnType<typeof featureStore.getState>;
+
+import { SamplePage } from "./pages/SamplePage";
+import { ExamplePage } from "./pages/ExamplePage";
+  
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <Provider store={featureStore}>
+      <PersistGate loading={null} persistor={featurePersistor}>
+        <Routes>
+          <Route path="sample-page" element={<SamplePage />} />
+          <Route path="example-page" element={<ExamplePage />} />
+          <Route path="*" element={<div>Error path</div>} />
+        </Routes>
+      </PersistGate>
+    </Provider>
+  );
+}
+export type { FeatureReduxState };
+
+export default App;
 
