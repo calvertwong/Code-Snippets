430fc8f67153915ec18740e0eb0644f05f5cb90c
Cal Vert W <31672796+calvertwong@users.noreply.github.com>
2023-11-06 15:05:31 -0800

    Update remove-image-pdf.py

--- remove-image-pdf.py
+++ remove-image-pdf.py
@@ -1,24 +1,39 @@
-# A sample code that can be used to remove images within a pdf file
+# A sample code that can be used to remove images within a pdf file (technically the existing image got replaced with a transparent image that is very small)
 import fitz
+import sys
 
-# Uncomment these two lines to use filepath from command line argument
-# filepath = sys.argv[1]
-# doc = fitz.open(filepath)
+# Reference: https://github.com/pymupdf/PyMuPDF-Utilities/blob/c80d3e8d7900451e4fa796407706e0c07caefcd3/examples/replace-image/replace.py#L18
+def img_replace(page, xref, filename=None, stream=None, pixmap=None):
+    if bool(filename) + bool(stream) + bool(pixmap) != 1:
+        raise ValueError("Exactly one of filename/stream/pixmap must be given")
+    doc = page.parent  # the owning document
+    new_xref = page.insert_image(
+        page.rect, filename=filename, stream=stream, pixmap=pixmap
+    )
+    doc.xref_copy(new_xref, xref)  # copy over new to old
+    last_contents_xref = page.get_contents()[-1]
+    doc.update_stream(last_contents_xref, b" ")
 
-doc = fitz.open("test.pdf")
+# get first argument of the execution command 
+filename = sys.argv[1]
 
-# focus on the last page of the pdf
-page = doc[len(doc)-1]
-imgName = "Im1"
-img = b"/%s Do" % imgName.encode()
-page.clean_contents()
-xref=page.get_contents()[0]
-cont=page.read_contents().splitlines()
+### READ IN PDF
+print(filename)
+# open pdf
+doc = fitz.open(filename)
+# Go to the last page of pdf
+page = doc[doc.page_count-1]
+# find images on page
+images = page.get_images()
+# perform this if at least one image exists
+if len(images) != 0:
+    item = images[0]
+    old_xref = item[0]
 
-for i in range(len(cont)):
-    str = cont[i].decode("utf-8") 
-    if cont[i] == img or '16 Tf' in str:
-            cont[i] = b""
-doc.update_stream(xref, b"\n".join(cont))
-page.clean_contents()
-doc.save("output.pdf", garbage=3, deflate=True)
+    pix = fitz.Pixmap(fitz.csGRAY, (0, 0, 1, 1), 1)
+    pix.clear_with()
+    img_replace(page, old_xref, pixmap=pix)
+
+    page.clean_contents()
+    doc.save("New_" + filename, garbage=3, deflate=True)
+    print("Image removed!!!")
